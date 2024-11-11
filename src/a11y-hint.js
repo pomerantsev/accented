649115e8a161d6688a2e44160ebb4f8875c27d50
Pavel Pomerantsev <pomerantsevp@gmail.com>
2024-11-11 07:46:25 -0500

    Decouple queue implementation from business logic

--- src/a11y-hint.js
+++ src/a11y-hint.js
@@ -1,11 +1,23 @@
-import scanRunner from './scan-runner.js';
+import axe from 'https://cdn.jsdelivr.net/npm/axe-core@4.10.2/+esm';
+import TaskQueue from './task-queue.js';
 
-scanRunner.add(document);
+const taskQueue = new TaskQueue(async () => {
+  performance.mark('axe-start');
+
+  const result = await axe.run();
+
+  const axeMeasure = performance.measure('axe', 'axe-start');
+
+  console.log('Result:', result);
+  console.log('Axe run duration:', Math.round(axeMeasure.duration), 'ms');
+});
+
+taskQueue.add(document);
 
 const mutationObserver = new MutationObserver(mutationList => {
   // TODO: filter out irrelevant mutations
   for (const mutationRecord of mutationList) {
-    scanRunner.add(mutationRecord.target);
+    taskQueue.add(mutationRecord.target);
   }
 });
 
---
+++ src/task-queue.js
@@ -0,0 +1,37 @@
+// TODO: add generic typing
+export default class TaskQueue {
+  items = new Set();
+
+  idleCallbackId = null;
+
+  asyncCallback = null;
+
+  constructor(asyncCallback) {
+    this.asyncCallback = asyncCallback;
+  }
+
+  // TODO: test all the asynchronicity
+
+  scheduleRun() {
+    if (this.idleCallbackId !== null || this.items.size === 0) {
+      return;
+    }
+
+    this.idleCallbackId = requestIdleCallback(() => this.run());
+  }
+
+  async run() {
+    this.items.clear();
+
+    await this.asyncCallback();
+
+    this.idleCallbackId = null;
+
+    this.scheduleRun();
+  }
+
+  add(item) {
+    this.items.add(item);
+    this.scheduleRun();
+  }
+}
