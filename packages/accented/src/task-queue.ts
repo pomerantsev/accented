0aa60c957669f74dd7c35dab62cad37ef47288c6
Pavel Pomerantsev <pomerantsevp@gmail.com>
2024-12-02 07:35:38 -0500

    Make some more fields private

--- packages/accented/src/task-queue.ts
+++ packages/accented/src/task-queue.ts
@@ -9,12 +9,12 @@ export default class TaskQueue<T> {
   // I'm not sure why the editor needs NodeJS.Timeout here.
   // tsconfig.json doesn't mention that the code needs to run in Node.
   // Maybe it's somehow related to the fact that we're using the Node test runner.
-  timeoutId: number | NodeJS.Timeout | null = null;
+  #timeoutId: number | NodeJS.Timeout | null = null;
 
-  asyncCallback: TaskCallback | null = null;
+  #asyncCallback: TaskCallback | null = null;
 
   constructor(asyncCallback: TaskCallback, {initialDelay, throttleDelay}: {initialDelay?: number, throttleDelay?: number} = {}) {
-    this.asyncCallback = asyncCallback;
+    this.#asyncCallback = asyncCallback;
     this.#initialDelay = initialDelay ?? 0;
     this.#throttleDelay = throttleDelay ?? 1000;
   }
@@ -23,23 +23,23 @@ export default class TaskQueue<T> {
     if (this.#items.size === 0) {
       this.#runningOrScheduled = false;
     }
-    if (this.timeoutId !== null || this.#items.size === 0) {
+    if (this.#timeoutId !== null || this.#items.size === 0) {
       return;
     }
 
     const delay = this.#runningOrScheduled ? this.#throttleDelay : this.#initialDelay;
     this.#runningOrScheduled = true;
-    this.timeoutId = setTimeout(() => this.#run(), delay);
+    this.#timeoutId = setTimeout(() => this.#run(), delay);
   }
 
   async #run() {
     this.#items.clear();
 
-    if (this.asyncCallback) {
-      await this.asyncCallback();
+    if (this.#asyncCallback) {
+      await this.#asyncCallback();
     }
 
-    this.timeoutId = null;
+    this.#timeoutId = null;
 
     this.#scheduleRun();
   }
