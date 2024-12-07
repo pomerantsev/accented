05daef0efad2dff6874fccf1c3dadd9b6764a793
Pavel Pomerantsev <pomerantsevp@gmail.com>
2024-12-06 21:32:05 -0500

    Improve state management

--- packages/accented/src/accented.ts
+++ packages/accented/src/accented.ts
@@ -1,8 +1,7 @@
-import axe from 'axe-core';
-import { effect } from '@preact/signals-core';
-import TaskQueue from './task-queue.js';
-import DomUpdater from './dom-updater.js';
-import issuesToElements from './utils/issuesToElements.js';
+
+import createDomUpdater from './dom-updater.js';
+import createLogger from './logger.js';
+import createScanner from './scanner.js';
 import { enabled } from './state.js';
 
 export type AccentedProps = {
@@ -42,57 +41,16 @@ export default function accented(props: Partial<AccentedProps> = {}): AccentedIn
   }
 
   enabled.value = true;
-
-  const domUpdater = new DomUpdater();
-
-  const taskQueue = new TaskQueue<Node>(async () => {
-    performance.mark('axe-start');
-
-    const result = await axe.run();
-
-    const axeMeasure = performance.measure('axe', 'axe-start');
-
-    if (!enabled.value) {
-      return;
-    }
-
-    const elements = issuesToElements(result.violations);
-    domUpdater.update(elements);
-
-    if (outputToConsole) {
-      console.log('Result:', result);
-    }
-    console.log('Axe run duration:', Math.round(axeMeasure.duration), 'ms');
-  }, { initialDelay, throttleDelay });
-
-  taskQueue.add(document);
-
-  const mutationObserver = new MutationObserver(mutationList => {
-    // TODO: filter out irrelevant mutations
-    for (const mutationRecord of mutationList) {
-      if (mutationRecord.type === 'attributes' && mutationRecord.attributeName === 'data-accented') {
-        continue;
-      }
-      taskQueue.add(mutationRecord.target);
-    }
-  });
-
-  effect(() => {
-    if (enabled.value) {
-      // TODO: read more about the params and decide which ones we need.
-      mutationObserver.observe(document, {
-        subtree: true,
-        childList: true,
-        attributes: true
-      });
-    } else {
-      mutationObserver.disconnect();
-    }
-  });
+  const cleanupScanner = createScanner(initialDelay, throttleDelay);
+  const cleanupDomUpdater = createDomUpdater();
+  const cleanupLogger = createLogger(outputToConsole);
 
   return {
     stop: () => {
       enabled.value = false;
+      cleanupScanner();
+      cleanupDomUpdater();
+      cleanupLogger();
     }
   };
 }
---
+++ packages/accented/src/scanner.ts
@@ -0,0 +1,47 @@
+import axe from 'axe-core';
+import TaskQueue from './task-queue.js';
+import issuesToElements from './utils/issuesToElements.js';
+import { effect } from '@preact/signals-core';
+import { enabled, elements } from './state.js';
+
+export default function createScanner(initialDelay: number, throttleDelay: number) {
+  const taskQueue = new TaskQueue<Node>(async () => {
+    performance.mark('axe-start');
+
+    const result = await axe.run();
+
+    const axeMeasure = performance.measure('axe', 'axe-start');
+
+    if (!enabled.value) {
+      return;
+    }
+
+    elements.value = issuesToElements(result.violations);
+
+    console.log('Axe run duration:', Math.round(axeMeasure.duration), 'ms');
+  }, { initialDelay, throttleDelay });
+
+  taskQueue.add(document);
+
+  const mutationObserver = new MutationObserver(mutationList => {
+    // TODO: filter out irrelevant mutations
+    for (const mutationRecord of mutationList) {
+      if (mutationRecord.type === 'attributes' && mutationRecord.attributeName === 'data-accented') {
+        continue;
+      }
+      taskQueue.add(mutationRecord.target);
+    }
+  });
+
+  return effect(() => {
+    // TODO: read more about the params and decide which ones we need.
+    mutationObserver.observe(document, {
+      subtree: true,
+      childList: true,
+      attributes: true
+    });
+    return () => {
+      mutationObserver.disconnect();
+    };
+  });
+}
