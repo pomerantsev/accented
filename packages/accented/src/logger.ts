3b2f869f1aed649323d7e7fbc5c3f32d225278c8
Pavel Pomerantsev <pomerantsevp@gmail.com>
2024-12-08 07:38:49 -0500

    Change array comparison function and write more tests

--- packages/accented/src/logger.ts
+++ packages/accented/src/logger.ts
@@ -1,11 +1,11 @@
 import { effect } from '@preact/signals-core';
 import { elements } from './state.js';
-import uniqueArraysContainSameElements from './utils/unique-arrays-contain-same-elements.js';
+import areSetsEqual from './utils/are-sets-equal.js';
 
 export default function createLogger(outputToConsole: boolean) {
   let previousElements: Array<Element> = [];
   return effect(() => {
-    if (outputToConsole && !uniqueArraysContainSameElements(previousElements, elements.value)) {
+    if (outputToConsole && !areSetsEqual(new Set(previousElements), new Set(elements.value))) {
       console.log('Elements:', elements.value);
     }
     previousElements = [...elements.value];
--- packages/accented/src/utils/unique-arrays-contain-same-elements.ts
+++
@@ -1,6 +0,0 @@
-export default function uniqueArraysContainSameElements<T>(array1: Array<T>, array2: Array<T>) {
-  const set1 = new Set(array1);
-  const set2 = new Set(array2);
-  return array1.every(item => set2.has(item)) &&
-    array2.every(item => set1.has(item));
-};
