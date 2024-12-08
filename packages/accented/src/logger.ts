587d0bb1f499467e224ff7ea15062da12a5de38a
Pavel Pomerantsev <pomerantsevp@gmail.com>
2024-12-08 07:42:41 -0500

    Ensure logger isn't created if outputToConsole is false

--- packages/accented/src/logger.ts
+++ packages/accented/src/logger.ts
@@ -2,10 +2,10 @@ import { effect } from '@preact/signals-core';
 import { elements } from './state.js';
 import areSetsEqual from './utils/are-sets-equal.js';
 
-export default function createLogger(outputToConsole: boolean) {
+export default function createLogger() {
   let previousElements: Array<Element> = [];
   return effect(() => {
-    if (outputToConsole && !areSetsEqual(new Set(previousElements), new Set(elements.value))) {
+    if (!areSetsEqual(new Set(previousElements), new Set(elements.value))) {
       console.log('Elements:', elements.value);
     }
     previousElements = [...elements.value];
