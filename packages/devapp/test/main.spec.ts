0411bf3b4556dbaad83436c558bb08087d026b06
Pavel Pomerantsev <pomerantsevp@gmail.com>
2024-12-09 04:02:45 -0500

    Introduce Playwright

---
+++ packages/devapp/test/main.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+const url = 'http://localhost:5173';
+
+test('has a primary heading', async ({ page }) => {
+  await page.goto(url);
+  await expect(page.getByRole('heading', { name: 'Accented test' })).toBeVisible();
+});
