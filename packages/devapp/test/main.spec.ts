4572b1002583ffdd43d3c07aef5d73c27b3964cf
Pavel Pomerantsev <pomerantsevp@gmail.com>
2024-12-14 10:45:42 -0500

    Add a few Playwright tests for the basic functionality

--- packages/devapp/test/main.spec.ts
+++ packages/devapp/test/main.spec.ts
@@ -2,7 +2,53 @@ import { test, expect } from '@playwright/test';
 
 const url = 'http://localhost:5173';
 
-test('has a primary heading', async ({ page }) => {
-  await page.goto(url);
-  await expect(page.getByRole('heading', { name: 'Accented test' })).toBeVisible();
+const accentedSelector = '[data-accented]';
+
+test.describe('Accented', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(url);
+  });
+
+  test.describe('basic functionality', () => {
+    test.describe('when enabled', () => {
+      test('adds an Accented-specific stylesheet to the document', async ({ page }) => {
+        const adoptedStyleSheets = await page.evaluate(() => document.adoptedStyleSheets);
+        await expect(adoptedStyleSheets).toHaveLength(1);
+      });
+
+      test('adds its attributes to elements', async ({ page }) => {
+        const count = await page.locator(accentedSelector).count();
+        await expect(count).toBeGreaterThan(0);
+      });
+
+      test('adds outlines with certain properties to elements', async ({ page }) => {
+        const buttonWithIssue = page.getByRole('button').and(page.locator(accentedSelector));
+        const [outlineWidth, outlineOffset] = await buttonWithIssue.evaluate(buttonElement => {
+          const computedStyle = window.getComputedStyle(buttonElement)
+          return [
+            computedStyle.getPropertyValue('outline-width'),
+            computedStyle.getPropertyValue('outline-offset')
+          ];
+        });
+        await expect(outlineWidth).toBe('2px');
+        await expect(outlineOffset).toBe('-2px');
+      });
+    });
+
+    test.describe('when disabled', () => {
+      test.beforeEach(async ({ page }) => {
+        await page.getByRole('button', { name: 'Toggle Accented' }).click();
+      });
+
+      test('removes the Accented-specific stylesheet from the document', async ({ page }) => {
+        const adoptedStyleSheets = await page.evaluate(() => document.adoptedStyleSheets);
+        await expect(adoptedStyleSheets).toHaveLength(0);
+      });
+
+      test('Accented-specific attributes are removed', async ({ page }) => {
+        const count = await page.locator(accentedSelector).count();
+        await expect(count).toBe(0);
+      });
+    });
+  });
 });
