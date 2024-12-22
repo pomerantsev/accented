905460b1a2bad55bd07ce748c03dcabb33aa022d
Pavel Pomerantsev <pomerantsevp@gmail.com>
2024-12-22 07:45:55 -0500

    Modify config again

--- packages/accented/release.config.mjs
+++ packages/accented/release.config.mjs
@@ -1,9 +1,13 @@
 export default {
   branches: ['main', { name: 'dev', prerelease: true }],
   plugins: [
-    ['@semantic-release/npm', {
+    '@semantic-release/commit-analyzer',
+    ['@semantic-release/exec', {
       prepareCmd: 'pnpm version ${nextRelease.version} --git-tag-version=false',
       publishCmd: 'pnpm publish --no-git-checks'
-    }]
+    }],
+    '@semantic-release/github',
+    '@semantic-release/npm',
+    '@semantic-release/release-notes-generator'
   ]
 };
