ed3f933b1abb94c25ba6215f20fa8586c40109e1
Pavel Pomerantsev <pomerantsevp@gmail.com>
2024-12-22 07:33:08 -0500

    Overwrite semantic-release commands to support pnpm

--- packages/accented/release.config.mjs
+++ packages/accented/release.config.mjs
@@ -1,3 +1,9 @@
 export default {
-  branches: ['main', { name: 'dev', prerelease: true }]
+  branches: ['main', { name: 'dev', prerelease: true }],
+  plugins: [
+    ['@semantic-release/exec', {
+      prepareCmd: 'pnpm version ${nextRelease.version} --git-tag-version=false',
+      publishCmd: 'pnpm publish --no-git-checks'
+    }]
+  ]
 };
