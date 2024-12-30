2d5dce8c2dc2a65fd6f2c0c63887d8740a3284e2
Pavel Pomerantsev <pomerantsevp@gmail.com>
2024-12-30 14:45:30 -0500

    Simplify obtaining elements by passing elementRef to axe.run

--- packages/accented/src/utils/issues-to-elements.ts
+++ packages/accented/src/utils/issues-to-elements.ts
@@ -5,12 +5,8 @@ export default function issuesToElements(issues: typeof AxeResults.violations) {
 
   for (const issue of issues) {
     for (const node of issue.nodes) {
-      // TODO: make this work for iframes and Shadow DOM
-      if (typeof node.target[0] === 'string') {
-        const element = document.querySelector(node.target[0]);
-        if (element) {
-          elements.add(element);
-        }
+      if (node.element) {
+        elements.add(node.element);
       }
     }
   }
