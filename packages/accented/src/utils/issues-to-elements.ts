5185b93c1dccba75ebe99251ba53d2e8cbcf611a
Pavel Pomerantsev <pomerantsevp@gmail.com>
2024-12-30 15:32:41 -0500

    Disallow iframe scanning

--- packages/accented/src/utils/issues-to-elements.ts
+++ packages/accented/src/utils/issues-to-elements.ts
@@ -5,7 +5,14 @@ export default function issuesToElements(issues: typeof AxeResults.violations) {
 
   for (const issue of issues) {
     for (const node of issue.nodes) {
-      if (node.element) {
+      // target.length > 1 means that the issue is in an iframe.
+      // Although axe-core can perform iframe scanning, I haven't succeeded in it,
+      // and the docs suggest that the axe-core script should be explicitly included
+      // in each of the iframed documents anyway.
+      // It seems preferable to disallow iframe scanning and not report issues in elements within iframes
+      // in the case that such issues are for some reason reported by axe-core.
+      // A consumer of Accented can instead scan the iframed document by calling Accented initialization from that document.
+      if (node.element && node.target.length === 1) {
         elements.add(node.element);
       }
     }
