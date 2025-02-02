30689dd5df29440af4a8b8c568b8ae818a4c1495
Pavel Pomerantsev <pomerantsevp@gmail.com>
2025-02-02 07:00:18 -0500

    Fix issue with disconnected elements

--- packages/accented/src/utils/update-elements-with-issues.ts
+++ packages/accented/src/utils/update-elements-with-issues.ts
@@ -34,29 +34,32 @@ export default function updateElementsWithIssues(extendedElementsWithIssues: Sig
         .filter(extendedElementWithIssues => {
           return !removedElementsWithIssues.some(removedElementWithIssues => removedElementWithIssues.element === extendedElementWithIssues.element);
         })
-        .concat(addedElementsWithIssues.map(addedElementWithIssues => {
-          const id = count++;
-          const trigger = win.document.createElement(`${name}-trigger`) as AccentedTrigger;
-          const elementZIndex = parseInt(win.getComputedStyle(addedElementWithIssues.element).zIndex, 10);
-          if (!isNaN(elementZIndex)) {
-            trigger.style.setProperty('z-index', (elementZIndex + 1).toString());
-          }
-          trigger.style.setProperty('position-anchor', `--${name}-anchor-${id}`);
-          trigger.dataset.id = id.toString();
-          const accentedDialog = win.document.createElement(`${name}-dialog`) as AccentedDialog;
-          trigger.dialog = accentedDialog;
-          const position = getElementPosition(addedElementWithIssues.element, win);
-          trigger.position = signal(position);
-          const issues = signal(addedElementWithIssues.issues);
-          accentedDialog.issues = issues;
-          return {
-            id,
-            element: addedElementWithIssues.element,
-            position: trigger.position,
-            trigger,
-            issues
-          };
-        }));
+        .concat(addedElementsWithIssues
+          .filter(addedElementWithIssues => addedElementWithIssues.element.isConnected)
+          .map(addedElementWithIssues => {
+            const id = count++;
+            const trigger = win.document.createElement(`${name}-trigger`) as AccentedTrigger;
+            const elementZIndex = parseInt(win.getComputedStyle(addedElementWithIssues.element).zIndex, 10);
+            if (!isNaN(elementZIndex)) {
+              trigger.style.setProperty('z-index', (elementZIndex + 1).toString());
+            }
+            trigger.style.setProperty('position-anchor', `--${name}-anchor-${id}`);
+            trigger.dataset.id = id.toString();
+            const accentedDialog = win.document.createElement(`${name}-dialog`) as AccentedDialog;
+            trigger.dialog = accentedDialog;
+            const position = getElementPosition(addedElementWithIssues.element, win);
+            trigger.position = signal(position);
+            const issues = signal(addedElementWithIssues.issues);
+            accentedDialog.issues = issues;
+            return {
+              id,
+              element: addedElementWithIssues.element,
+              position: trigger.position,
+              trigger,
+              issues
+            };
+          })
+        );
     }
   });
 }
