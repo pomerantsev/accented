081e17a8928c9ad42afbc524105d07621607fb98
Pavel Pomerantsev <pomerantsevp@gmail.com>
2025-02-18 07:55:46 -0500

    Replace test import in bundlerless spec

--- packages/devapp/test/bundlerless.spec.ts
+++ packages/devapp/test/bundlerless.spec.ts
@@ -1,4 +1,5 @@
-import { test, expect } from '@playwright/test';
+import { test } from './fixtures/test';
+import { expect } from '@playwright/test';
 
 const accentedDataAttr = 'data-accented';
 const accentedSelector = `[${accentedDataAttr}]`;
