6e93f549744e1973c58a3852d00d3bed47a4d7c4
Pavel Pomerantsev <pomerantsevp@gmail.com>
2025-02-25 08:03:00 -0500

    Fix: element positioning within transformed elements
    https://achrafkassioui.com/blog/position-fixed-and-CSS-transforms/

--- packages/accented/src/utils/get-element-position.ts
+++ packages/accented/src/utils/get-element-position.ts
@@ -1,18 +1,45 @@
 import type { Position } from '../types';
 
+function getNearestTransformedAncestor(element: Element, win: Window): Element | null {
+  let currentElement: Element | null = element;
+  while (currentElement) {
+    const transform = win.getComputedStyle(currentElement).transform;
+    if (transform !== 'none') {
+      return currentElement;
+    }
+    currentElement = currentElement.parentElement;
+  }
+  return null;
+}
+
 export default function getElementPosition(element: Element, win: Window): Position {
   const rect = element.getBoundingClientRect();
+  const transformedAncestor = getNearestTransformedAncestor(element, win);
+  let top, left, right;
+  // If an element has an ancestor whose transform is not 'none',
+  // fixed positioning works differently.
+  // https://achrafkassioui.com/blog/position-fixed-and-CSS-transforms/
+  if (transformedAncestor) {
+    const transformedAncestorRect = transformedAncestor.getBoundingClientRect();
+    top = rect.top - transformedAncestorRect.top;
+    left = rect.left - transformedAncestorRect.left;
+    right = rect.right - transformedAncestorRect.left;
+  } else {
+    top = rect.top;
+    left = rect.left;
+    right = rect.right;
+  }
   const direction = win.getComputedStyle(element).direction;
   if (direction === 'ltr') {
     return {
-      inlineEndLeft: rect.right,
-      blockStartTop: rect.top,
+      inlineEndLeft: right,
+      blockStartTop: top,
       direction
     };
   } else if (direction === 'rtl') {
     return {
-      inlineEndLeft: rect.left,
-      blockStartTop: rect.top,
+      inlineEndLeft: left,
+      blockStartTop: top,
       direction
     };
   } else {
