45d7b1ebe56a5f28fa1e6399682500bfcf45b413
Pavel Pomerantsev <pomerantsevp@gmail.com>
2025-04-02 07:48:37 -0400

    Implement context correctly

--- packages/accented/src/utils/is-node-in-scan-context.ts
+++ packages/accented/src/utils/is-node-in-scan-context.ts
@@ -1,7 +1,29 @@
+/* Mostly copied from https://github.com/dequelabs/axe-core/blob/develop/lib/core/utils/is-node-in-context.js */
+
 import type { ScanContext } from '../types';
 import contains from './contains.js';
+import ensureNonEmpty from './ensure-non-empty.js';
 
-// TODO: this implementation is incomplete
-export default function isNodeInScanContext(node: Node, scanContext: ScanContext) {
-  return scanContext.include.some(includedNode => contains(includedNode, node));
+function getDeepest(nodes: [Node, ...Node[]]): Node {
+  let deepest = nodes[0];
+  for (const node of nodes.slice(1)) {
+    if (!contains(node, deepest)) {
+      deepest = node;
+    }
+  }
+  return deepest;
+}
+
+export default function isNodeInScanContext(node: Node, { include, exclude }: ScanContext): boolean {
+  const filterInclude = include.filter(candidate => contains(candidate, node));
+  if (filterInclude.length === 0) {
+    return false;
+  }
+  const filterExcluded = exclude.filter(candidate => contains(candidate, node));
+  if (filterExcluded.length === 0) {
+    return true;
+  }
+  const deepestInclude = getDeepest(ensureNonEmpty(filterInclude));
+  const deepestExclude = getDeepest(ensureNonEmpty(filterExcluded));
+  return contains(deepestExclude, deepestInclude);
 }
