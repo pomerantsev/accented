bbad8b6c84c50f9c086549a42ff153be4009c213
Pavel Pomerantsev <pomerantsevp@gmail.com>
2025-04-06 07:40:15 -0400

    Write tests for contains.ts

---
+++ packages/accented/src/utils/contains.test.ts
@@ -0,0 +1,55 @@
+import { JSDOM } from 'jsdom';
+import assert from 'node:assert/strict';
+import { suite, test } from 'node:test';
+import contains from './contains';
+
+suite('contains', () => {
+  test('an element contains itself', () => {
+    const dom = new JSDOM('<div id="test"></div>');
+    const { document } = dom.window;
+    const element = document.querySelector('#test')!;
+
+    assert.equal(contains(element, element), true);
+  });
+
+  test('an element does not contain its sibling', () => {
+    const dom = new JSDOM('<div><div id="sibling1"></div><div id="sibling2"></div></div>');
+    const { document } = dom.window;
+    const sibling1 = document.querySelector('#sibling1')!;
+    const sibling2 = document.querySelector('#sibling2')!;
+
+    assert.equal(contains(sibling1, sibling2), false);
+  });
+
+  test('an element contain its descendant', () => {
+    const dom = new JSDOM('<div id="ancestor"><div id="descendant"></div></div>');
+    const { document } = dom.window;
+    const ancestor = document.querySelector('#ancestor')!;
+    const descendant = document.querySelector('#descendant')!;
+
+    assert.equal(contains(ancestor, descendant), true);
+  });
+
+  test('an element contain its descendant', () => {
+    const dom = new JSDOM('<div id="ancestor"><div id="descendant"></div></div>');
+    const { document } = dom.window;
+    const ancestor = document.querySelector('#ancestor')!;
+    const descendant = document.querySelector('#descendant')!;
+
+    assert.equal(contains(descendant, ancestor), false);
+  });
+
+  test('an element contain its descendant if the descendant is in a shadow DOM', () => {
+    const dom = new JSDOM('<div id="ancestor"><div id="host"></div></div>');
+    global.Node = dom.window.Node;
+    const { document } = dom.window;
+    const ancestor = document.querySelector('#ancestor')!;
+    const host = document.querySelector('#host')!;
+    const shadowRoot = host.attachShadow({ mode: 'open' });
+    shadowRoot.innerHTML = '<div id="descendant"></div>';
+    const descendant = shadowRoot.querySelector('#descendant')!;
+    console.log(descendant);
+
+    assert.equal(contains(ancestor, descendant), true);
+  });
+});
