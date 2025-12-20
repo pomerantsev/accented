6296d39e0ade94f894e1ff3ef5cd7d568ff6290d
Pavel Pomerantsev <pomerantsevp@gmail.com>
2025-12-20 09:24:57 -0500

    Output more data in the action

--- packages/website/src/actions/index.ts
+++ packages/website/src/actions/index.ts
@@ -1,15 +1,30 @@
 import { defineAction } from 'astro:actions';
 import { z } from 'astro:schema';
+import { UAParser } from 'ua-parser-js';
 
 export const server = {
   collectMetrics: defineAction({
     input: z.object({
       lcp: z.number(),
+      commitSha: z.string(),
     }),
     handler: async (input, { request }) => {
-      console.log('Action input:', input);
-      console.log('Action request headers:', request.headers);
-      return `LCP: ${input.lcp}`;
+      const { browser, os } = UAParser(request.headers.get('user-agent') ?? '');
+      const data = {
+        ...input,
+        browser: {
+          name: browser.name,
+          major: browser.major,
+        },
+        os: {
+          name: os.name,
+          version: os.version,
+        },
+        createdAt: Date.now(),
+        // TODO: strip query string? https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Referer
+        url: request.headers.get('referer') ?? '',
+      };
+      console.log(JSON.stringify(data, null, 2));
     },
   }),
 };
