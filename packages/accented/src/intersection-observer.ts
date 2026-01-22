ef21e723d5566dd7750b47255544f82a3dca63ec
Pavel Pomerantsev <pomerantsevp@gmail.com>
2026-01-21 22:17:57 -0500

    Add comment on position-visibility (#431)
    Partially addresses #424

--- packages/accented/src/intersection-observer.ts
+++ packages/accented/src/intersection-observer.ts
@@ -12,11 +12,27 @@ export function setupIntersectionObserver() {
             (el) => el.element === entry.target,
           );
           if (extendedElementWithIssues) {
-            // We initially treated setting visibility in the intersection observer
-            // as a fallback option for browsers that don't support `position-visibility`,
-            // but then we realized that this `position-visibility` actually works
-            // in an unexpected way when the container has `overflow: visible`.
-            // So now we always set visibility in the intersection observer.
+            /* We initially treated setting visibility in the intersection observer
+               as a fallback option for browsers that don't support `position-visibility`,
+               but then we realized that this `position-visibility` actually works
+               in an unexpected way when the container has `overflow: visible`.
+               So now we always set visibility in the intersection observer.
+
+               Update (Jan 2026): the original Chromium issue is fixed:
+               https://issues.chromium.org/issues/425901169
+
+               However, there's another issue that makes me hesitant to use `position-visibility`.
+               According to the spec (https://www.w3.org/TR/css-anchor-position-1/#position-visibility),
+               when the element is hidden with `position-visibility`, its `visibility` property computes
+               to `force-hidden`. That, however, is not implemented in any of the browsers
+               at the time of this writing (the computed value of `visibility` is still `visible`),
+               which seems to be the reason why Playwright is still reporting those elements as visible
+               (and it doesn't seem possible to automatically determine if they're visible or not).
+
+               Until that's properly implemented, we're probably better off relying on an intersection observer
+               (and having the visibility state testable).
+            */
+
             extendedElementWithIssues.visible.value = entry.isIntersecting;
             if (entry.isIntersecting && !supportsAnchorPositioning()) {
               extendedElementWithIssues.position.value = getElementPosition(entry.target);
