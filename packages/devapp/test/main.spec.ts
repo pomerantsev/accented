pomerantsev/accented

commit 04a44c7a05716d319bc1aaa6a9f250cc47503c9f

Fix positioning of Accented triggers on transformed elements in Chrome 144+ (#443)

packages/accented/src/utils/css-transforms.ts
@@ -1,0 +1,42 @@
+/**
+ * Some browsers (at the time of this writing, Chrome 144+) started to take CSS transforms into account
+ * when calculating the position of the anchored element.
+ *
+ * In light of this, this utility helps determine if we need to apply a separate transform to the anchored element
+ * when it's added to the DOM.
+ */
+
+let doesAffect: boolean;
+
+export function cssTransformsAffectAnchorPositioning() {
+  return doesAffect;
+}
+
+/**
+ * This function is supposed to run once at library initialization.
+ * It creates two elements (an anchor and an anchored element),
+ * with a transform applied to the anchor, and determines whether
+ * their positions still match.
+ */
+export function initializeCssTransformsCheck() {
+  const anchor = document.createElement('div');
+  anchor.style.inlineSize = '100px';
+  anchor.style.blockSize = '100px';
+  anchor.style.transform = 'translateX(100px)';
+  // @ts-expect-error TS is unaware of `anchor-name`
+  anchor.style.anchorName = '--test-anchor';
+
+  const anchored = document.createElement('div');
+  anchored.style.inlineSize = '10px';
+  anchored.style.blockSize = '10px';
+  anchored.style.position = 'absolute';
+  // @ts-expect-error TS is unaware of `position-anchor`
+  anchored.style.positionAnchor = '--test-anchor';
+  anchored.style.insetInlineStart = 'anchor(start)';
+  anchored.style.insetBlockStart = 'anchor(start)';
+
+  document.body.appendChild(anchor);
+  document.body.appendChild(anchored);
+
+  doesAffect = anchor.getBoundingClientRect().left === anchored.getBoundingClientRect().left;
+}

packages/accented/src/accented.ts
@@ -10,6 +10,7 @@ import { setupScrollListeners } from './scroll-listeners.js';
 import { enabled, extendedElementsWithIssues } from './state.js';
 import type { AccentedOptions, DisableAccented } from './types.ts';
 import { initializeContainingBlockSupportSet } from './utils/containing-blocks.js';
+import { initializeCssTransformsCheck } from './utils/css-transforms.js';
 import { deepMerge } from './utils/deep-merge.js';
 import { supportsAnchorPositioning } from './utils/supports-anchor-positioning.js';
 import { validateOptions } from './validate-options.js';
@@ -92,6 +93,7 @@ export function accented(options: AccentedOptions = {}): DisableAccented {
     enabled.value = true;
 
     initializeContainingBlockSupportSet();
+    initializeCssTransformsCheck();
     registerElements(name);
 
     const { disconnect: cleanupIntersectionObserver, intersectionObserver } =

packages/accented/src/elements/accented-trigger.ts
@@ -3,6 +3,7 @@ import { effect } from '@preact/signals-core';
 import { fontSystemSans } from '../common/tokens.js';
 import { logAndRethrow } from '../log-and-rethrow.js';
 import type { Position } from '../types.ts';
+import { cssTransformsAffectAnchorPositioning } from '../utils/css-transforms.js';
 import { supportsAnchorPositioning } from '../utils/supports-anchor-positioning.js';
 import type { AccentedDialog } from './accented-dialog.ts';
 
@@ -241,6 +242,9 @@ export const getAccentedTrigger = (name: string) => {
     }
 
     #setTransform() {
+      if (cssTransformsAffectAnchorPositioning()) {
+        return;
+      }
       // We read and write values in separate animation frames to avoid layout thrashing.
       window.requestAnimationFrame(() => {
         if (this.element) {

packages/devapp/test/helpers/trigger.ts
@@ -45,8 +45,4 @@ export async function expectElementAndTriggerToBeAligned(
   expect(triggerRect[side]).toBeLessThan(elementRect[side] + 2);
   expect(triggerRect.top).toBeGreaterThan(elementRect.top - 2);
   expect(triggerRect.top).toBeLessThan(elementRect.top + 2);
-
-  const elementTransform = await getTransform(element);
-  const triggerContainerTransform = await getTransform(triggerContainer);
-  expect(elementTransform).toBe(triggerContainerTransform);
 }

commit fa3688f564c8dd60b272bbc3c4239764b54c6d1d

Fix: Accented was adding extraneous elements to the page
This is a fix for a regression that was introduced in PR #443: the two
elements that were added to the body for feature testing purposes were
never removed.

packages/accented/src/utils/css-transforms.ts
@@ -39,4 +39,7 @@ export function initializeCssTransformsCheck() {
   document.body.appendChild(anchored);
 
   doesAffect = anchor.getBoundingClientRect().left === anchored.getBoundingClientRect().left;
+
+  anchor.remove();
+  anchored.remove();
 }

packages/devapp/test/main.spec.ts
@@ -105,6 +105,20 @@ test.describe('Accented', () => {
         // so the issue will be reported.
         expect(await page.locator(`#correctly-structured-list${accentedSelector}`)).toBeVisible();
       });
+
+      test('doesn’t introduce new elements that aren’t triggers', async ({ page }) => {
+        await page.goto('?disable');
+        const initialElementCount = await page.evaluate(
+          () => document.querySelectorAll('*').length,
+        );
+        await page.getByRole('button', { name: 'Toggle Accented' }).click();
+        const triggerCount = await page.evaluate(
+          () => document.querySelectorAll('accented-trigger').length,
+        );
+        expect(triggerCount).toBeGreaterThan(0);
+        const finalElementCount = await page.evaluate(() => document.querySelectorAll('*').length);
+        expect(finalElementCount).toBe(initialElementCount + triggerCount);
+      });
     });
 
     test.describe('when disabled', () => {